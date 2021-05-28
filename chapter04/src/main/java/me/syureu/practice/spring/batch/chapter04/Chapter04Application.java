9d1f66067b0de09eb3fc3190d26cb327d873fd50
Syureu <tor01237@gmail.com>
2021-05-28 17:19:01 +0900

    add @EnableBatchProcessing, step, job
    * @EnableBatchProcessing is due to hide batch boilerplate.
    * It offer a lot of spring bean definition for batch infrastructure, so you don't need to inject below components.
    * JobRepository, JobLauncher, JobExplorer, JobRegistry, PlatformTransactionManger, JobBuilderFactory, StepBuilderFactory
    
    * JobBuilderFactory -> Job Builder
    * StepBuilderFactory -> Step Builder

--- chapter04/src/main/java/me/syureu/practice/spring/batch/chapter04/Chapter04Application.java
+++ chapter04/src/main/java/me/syureu/practice/spring/batch/chapter04/Chapter04Application.java
@@ -1,10 +1,43 @@
 package me.syureu.practice.spring.batch.chapter04;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class Chapter04Application {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public Chapter04Application(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("Hello, world!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Job job() {
+        return this.jobBuilderFactory.get("basicJob")
+                .start(step1())
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Chapter04Application.class, args);
