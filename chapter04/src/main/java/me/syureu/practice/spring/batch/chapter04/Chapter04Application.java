cf95c60f983006f30a840096a0566e6a4429c696
Syureu <tor01237@gmail.com>
2021-05-31 21:30:19 +0900

    CompositeJobParametersValidator를 이용해 여러 validator를 동시에 사용 가능
    DefaultJobParametersValidator의 afterPropertiesSet메서드는 말그대로 프로퍼티들(requiredKeys, OptionalKeys)을 셋팅해주고 실행하면 되는 메서드인데
    내부적으로 RK와 OK가 겹치는지 검사하고 겹치는 경우 Assert함.

--- chapter04/src/main/java/me/syureu/practice/spring/batch/chapter04/Chapter04Application.java
+++ chapter04/src/main/java/me/syureu/practice/spring/batch/chapter04/Chapter04Application.java
@@ -1,12 +1,12 @@
 package me.syureu.practice.spring.batch.chapter04;
 
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.CompositeJobParametersValidator;
 import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -14,6 +14,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
 
 @EnableBatchProcessing
 @SpringBootApplication
@@ -28,12 +31,13 @@ public class Chapter04Application {
     }
 
     @Bean
-    public JobParametersValidator validator() {
-        DefaultJobParametersValidator validator = new DefaultJobParametersValidator();
+    public CompositeJobParametersValidator validator() {
+        CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
 
-        validator.setRequiredKeys(new String[]{"fileName"});
-        validator.setOptionalKeys(new String[]{"name"});
+        DefaultJobParametersValidator defaultJobParametersValidator = new DefaultJobParametersValidator(new String[]{"fileName"}, new String[]{"name"});
+        defaultJobParametersValidator.afterPropertiesSet();
 
+        validator.setValidators(Arrays.asList(new ParameterValidator(), defaultJobParametersValidator));
         return validator;
     }
 
