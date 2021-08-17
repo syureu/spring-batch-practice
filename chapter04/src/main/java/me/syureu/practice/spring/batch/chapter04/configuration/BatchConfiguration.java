49251f77aa40ecdd59bc44d0a388f46910b1c290
Syureu <tor01237@gmail.com>
2021-08-17 15:46:57 +0900

    ExecutionContext 저장하기
    stepContext의 stepExecutionContext를 꺼내서 name을 넣었으므로 db의 BATCH_STEP_EXECUTION_CONTEXT에서 확인가능, 책은 뭔가 잘못 섞어서 설명중인듯

--- chapter04/src/main/java/me/syureu/practice/spring/batch/chapter04/configuration/BatchConfiguration.java
+++ chapter04/src/main/java/me/syureu/practice/spring/batch/chapter04/configuration/BatchConfiguration.java
@@ -4,12 +4,17 @@ import me.syureu.practice.spring.batch.chapter04.tasklet.GoodByeTasklet;
 import me.syureu.practice.spring.batch.chapter04.tasklet.HelloTasklet;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.listener.ExecutionContextPromotionListener;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -29,31 +34,35 @@ public class BatchConfiguration {
     public Job job() {
         return jobBuilderFactory.get("job")
                 .start(step1())
-                .next(step2())
                 .build();
     }
 
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet(new HelloTasklet())
-                .listener(promotionListener())
+                .tasklet(new HelloWorld())
                 .build();
     }
 
-    @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
-                .tasklet(new GoodByeTasklet())
-                .build();
+    public static class HelloWorld implements Tasklet {
+
+        private static final String HELLO_WORLD = "Hello, %s\n";
+
+        @Override
+        public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+            String name = (String) chunkContext.getStepContext()
+                    .getJobParameters()
+                    .get("name");
+
+            ExecutionContext jobContext = chunkContext.getStepContext()
+                    .getStepExecution()
+                    .getExecutionContext();
+            jobContext.put("name", name);
+
+            System.out.printf(HELLO_WORLD, name);
+
+            return RepeatStatus.FINISHED;
+        }
     }
 
-    @Bean
-    public StepExecutionListener promotionListener() {
-        ExecutionContextPromotionListener listener = new ExecutionContextPromotionListener();
-
-        listener.setKeys(new String[]{"name"});
-
-        return listener;
-    }
 }
